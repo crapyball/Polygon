2ffb8063b97194fb25738ac4d1ba22a5924e3afe
crapyball <nedons@cps.edu>
2025-11-13 18:25:19 -0600

    continuing to finish angles

--- Polygon/main.cpp
+++ Polygon/main.cpp
@@ -43,42 +43,41 @@ void connect2Vertices(Vertice one, Vertice two, vector<vector<string>>& canvas)
     int deltaz = two.z - one.z;
 
 
-    int upslash_count = 0;
-    int backslash_count = 0;
+    int shift_count = 0;
     int dash_count = 0;
-    int slash_increment = 0;
+    int shift_increment = 0;
     int vert_bar_count = 0;
 
     //Quadrant 1
     if (deltax > 0 && deltay > 0)
     {
-        //less than 45 degrees.
+        //less than 45 degrees.(0 to 45)
         if (deltax >= deltay)
         {
-            upslash_count = deltay;
-            dash_count = deltax - upslash_count;
-            slash_increment = deltax / deltay;
+            shift_count = deltay;
+            dash_count = deltax - shift_count;
+            shift_increment = deltax / deltay;
 
             int xinc = 1;//starts at 1 because we start with a horizontal bar
             int yinc = 0; // starts at 0 because we start with a horizontal bar
-            //counter to mark when a slash should be entered
-            int counter = 1;
+            //shift_counter to mark when a slash should be entered
+            int shift_counter = 1;
             for (int i = 0; i < deltax; i++)
             {
-                if (counter < slash_increment)
+                if (shift_counter < shift_increment)
                 {
                     canvas[one.x + xinc][one.y + yinc] = "-";
 
                     xinc += 1;
-                    counter += 1;
+                    shift_counter += 1;
 
                 }
-                else if (counter >= slash_increment)
+                else if (shift_counter >= shift_increment)
                 {
                     yinc += 1;
                     canvas[one.x + xinc][one.y + yinc] = "-";
                     xinc += 1;
-                    counter = 1;
+                    shift_counter = 1;
 
                 }
                 cout << "deltay" << deltay << endl;
@@ -88,33 +87,33 @@ void connect2Vertices(Vertice one, Vertice two, vector<vector<string>>& canvas)
             }
         }
         
-        //greater than 45 degrees
+        //greater than 45 degrees (45 to 90)
         if (deltay > deltax)
         {
-            upslash_count = deltax;
-            vert_bar_count = deltay - upslash_count;
-            slash_increment = deltay / deltax;
+            shift_count = deltax;
+            vert_bar_count = deltay - shift_count;
+            shift_increment = deltay / deltax;
 
-            int xinc = 0;//starts at 0 because we start with a vertical bar
-            int yinc = 1; // starts at 1 because we start with a vertical bar
-            //counter to mark when a slash should be entered
-            int counter = 1;
+            int xinc = 0;
+            int yinc = 1; 
+            
+            int shift_counter = 1;
             for (int i = 0; i < deltay; i++)
             {
-                if (counter < slash_increment)
+                if (shift_counter < shift_increment)
                 {
                     canvas[one.x + xinc][one.y + yinc] = "|";
 
                     yinc += 1;
-                    counter += 1;
+                    shift_counter += 1;
 
                 }
-                else if (counter >= slash_increment)
+                else if (shift_counter >= shift_increment)
                 {
                     xinc += 1;
                     canvas[one.x + xinc][one.y + yinc] = "|";
                     yinc += 1;
-                    counter = 1;
+                    shift_counter = 1;
 
                 }
                 cout << "deltay" << deltay << endl;
@@ -130,34 +129,34 @@ void connect2Vertices(Vertice one, Vertice two, vector<vector<string>>& canvas)
     //Quadrant 2
     if (deltax > 0 && deltay < 0)
     {
-        //greater than -45 degrees
+        //greater than -45 degrees (0 to -45)
         if (deltax >= abs(deltay))
         {
-            backslash_count = deltay;
-            dash_count = abs(deltax - backslash_count);
-            slash_increment = abs(deltax / deltay);
+            shift_count = deltay;
+            dash_count = abs(deltax - shift_count);
+            shift_increment = abs(deltax / deltay);
             
 
-            int xinc = 1;//starts at 1 because we start with a horizontal bar
-            int yinc = 0; // starts at 0 because we start with a horizontal bar
-            //counter to mark when a slash should be entered
-            int counter = 1;
+            int xinc = 1;
+            int yinc = 0; 
+            
+            int shift_counter = 1;
             for (int i = 0; i < deltax; i++)
             {
-                if (counter < slash_increment)
+                if (shift_counter < shift_increment)
                 {
                     canvas[one.x + xinc][one.y - yinc] = "-";
 
                     xinc += 1;
-                    counter += 1;
+                    shift_counter += 1;
 
                 }
-                else if (counter >= slash_increment)
+                else if (shift_counter >= shift_increment)
                 {
                     yinc += 1;
                     canvas[one.x + xinc][one.y - yinc] = "-";
                     xinc += 1;
-                    counter = 1;
+                    shift_counter = 1;
 
                 }
                 cout << "deltay" << deltay << endl;
@@ -166,33 +165,33 @@ void connect2Vertices(Vertice one, Vertice two, vector<vector<string>>& canvas)
 
             }
         }
-        // less than -45 degrees
+        // less than -45 degrees (-45 to -90)
         if (abs(deltay) > deltax)
         {
-            backslash_count = deltax;
-            vert_bar_count = abs(deltay) - backslash_count;
-            slash_increment = abs(deltay / deltax);
+            shift_count = deltax;
+            vert_bar_count = abs(deltay) - shift_count;
+            shift_increment = abs(deltay / deltax);
 
-            int xinc = 0;//starts at 0 because we start with a vertical bar
-            int yinc = 1; // starts at 1 because we start with a vertical bar
-            //counter to mark when a slash should be entered
-            int counter = 1;
+            int xinc = 0;
+            int yinc = 1; 
+          
+            int shift_counter = 1;
             for (int i = 0; i < abs(deltay); i++)
             {
-                if (counter < slash_increment)
+                if (shift_counter < shift_increment)
                 {
                     canvas[one.x + xinc][one.y - yinc] = "|";
 
                     yinc += 1;
-                    counter += 1;
+                    shift_counter += 1;
 
                 }
-                else if (counter >= slash_increment)
+                else if (shift_counter >= shift_increment)
                 {
                     xinc += 1;
                     canvas[one.x + xinc][one.y - yinc] = "|";
                     yinc += 1;
-                    counter = 1;
+                    shift_counter = 1;
 
                 }
                 cout << "deltay" << deltay << endl;
@@ -201,22 +200,91 @@ void connect2Vertices(Vertice one, Vertice two, vector<vector<string>>& canvas)
 
             }
 
-            cout << "upslash count: " << upslash_count << endl;
-            cout << "backslash count: " << backslash_count << endl;
-            cout << "dash coun: " << dash_count << endl;
-            cout << "slash increment: " << slash_increment << endl;
-            cout << "vert bar count: " << vert_bar_count << endl;
+            
         }
 
         
     }
 
+    //Quadrant 3
+    if (deltax < 0 && deltay < 0)
+    {
+        //less than -145 (-90 to -145)
+        if (abs(deltax) < abs(deltay))
+        {
+            shift_count = abs(deltax);
+            vert_bar_count = abs(deltay) - shift_count;
+            shift_increment = abs(deltay / deltax);
+
+            int xinc = 0;
+            int yinc = 1; 
+            
+            int shift_counter = 1;
+            for (int i = 0; i < abs(deltay); i++)
+            {
+                if (shift_counter < shift_increment)
+                {
+                    canvas[one.x - xinc][one.y - yinc] = "|";
+
+                    yinc += 1;
+                    shift_counter += 1;
+
+                }
+                else if (shift_counter >= shift_increment)
+                {
+                    xinc += 1;
+                    canvas[one.x - xinc][one.y - yinc] = "|";
+                    yinc += 1;
+                    shift_counter = 1;
+
+                }
+                cout << "deltay" << deltay << endl;
+                cout << "xinc" << xinc << endl;
+                cout << "yinc" << yinc << endl;
+            }
+        }
+        // less than -145 degrees (-145 to -180)
+        if (abs(deltay) < abs(deltax))
+        {
+            shift_count = deltay;
+            dash_count = deltax - shift_count;
+            shift_increment = deltax / deltay;
+
+            int xinc = 1;//starts at 1 because we start with a horizontal bar
+            int yinc = 0; // starts at 0 because we start with a horizontal bar
+            //shift_counter to mark when a slash should be entered
+            int shift_counter = 1;
+            for (int i = 0; i < deltax; i++)
+            {
+                if (shift_counter < shift_increment)
+                {
+                    canvas[one.x - xinc][one.y - yinc] = "-";
+
+                    xinc += 1;
+                    shift_counter += 1;
+
+                }
+                else if (shift_counter >= shift_increment)
+                {
+                    yinc += 1;
+                    canvas[one.x - xinc][one.y - yinc] = "-";
+                    xinc += 1;
+                    shift_counter = 1;
+
+                }
+                cout << "deltay" << deltay << endl;
+                cout << "xinc" << xinc << endl;
+                cout << "yinc" << yinc << endl;
+            }
+        }
+    }
+
 }
 /*
-cout << "upslash count: " << upslash_count << endl;
-            cout << "backslash count: " << backslash_count << endl;
+cout << "upslash count: " << shift_count << endl;
+            cout << "backslash count: " << shift_count << endl;
             cout << "dash coun: " << dash_count << endl;
-            cout << "slash increment: " << slash_increment << endl;
+            cout << "slash increment: " << shift_increment << endl;
             cout << "vert bar count: " << vert_bar_count << endl;
 */
  
@@ -234,7 +302,7 @@ int main()
     initializeCanvas(canvas, sizex, sizey);
     
 
-    Polygon triangle = Polygon(vector<Vertice> {Vertice(25, 25, 0), Vertice(25, 0, 0), Vertice(30, 0, 10) });
+    Polygon triangle = Polygon(vector<Vertice> {Vertice(25, 25, 0), Vertice(25, 0, 0), Vertice(10, 20, 10) });
     connect2Vertices(triangle.vertices[0], triangle.vertices[2], canvas);
     printCanvas(canvas, sizex, sizey);
 }
